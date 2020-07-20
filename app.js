804e3a69b79a0a1f3d663855d091a5f638cead54
Ryan <ryansherring@gmail.com>
2020-07-19 23:06:45 -0700

    deployment

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 function ccConfirm(){
-    // CC NUMBER
+    // CC NUMBER for modal
     var field1 = document.getElementById('part1').value;
     var field2 = document.getElementById('part2').value;
     var field3 = document.getElementById('part3').value;
     var field4 = document.getElementById('part4').value;
     var cardNumber = `${field1}-${field2}-${field3}-${field4}`
-    document.getElementById('ccnumconfirm').innerHTML=cardNumber;
+    document.getElementById('ccnumconfirm').innerHTML += cardNumber;
 
-    // CC DATE
+    // CC DATE for modal
     var month = document.getElementById('month').value;
     var year = document.getElementById('year').value;
     var expirationDate = `${month}, ${year}`
-    document.getElementById('dateconfirm').innerHTML=expirationDate;
+    document.getElementById('dateconfirm').innerHTML += expirationDate;
     
     
-    // CC CVV
+    // CC CVV for modal
     var cvv = document.getElementById('cvv').value;
-    document.getElementById('cvvconfirm').innerHTML=cvv;
+    document.getElementById('cvvconfirm').innerHTML += cvv;
 }
 
 
